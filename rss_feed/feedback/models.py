44eda232deb0632997dc61e5a1219ac5616e956f
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-10-10 23:47:40 +0330

    chore: like model using contenttype to use the model one for both podcast and episode

--- rss_feed/feedback/models.py
+++ rss_feed/feedback/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Like(BaseModel):
+    account = models.ForeignKey(User,on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
 
-# Create your models here.
+    def __str__(self):
+        return self.account
+
