1cfe1eb3ca59be5dc93cf3632ca6fde14df9be83
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-10-10 23:46:07 +0330

    chore: registering feedback models

--- rss_feed/feedback/admin.py
+++ rss_feed/feedback/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Like, Comment, Playlist
+
 # Register your models here.
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Playlist)
