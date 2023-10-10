c062a4af5dad4a61df77622a4bbc459e547af7d4
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-10-10 23:36:08 +0330

    fix: removing phone from fields and removing redundant configuration

--- rss_feed/account/models.py
+++ rss_feed/account/models.py
@@ -1,53 +1,31 @@
 from django.db import models
-# from django.contrib import auth
 from django.core.mail import send_mail
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
 from account.manager import MyUserManager
+from . import jwt_tools
+
 
 # Create your models here.
 class User(AbstractBaseUser, PermissionsMixin):
 
-    phone = models.CharField(max_length=12)
-    image = models.ImageField(upload_to="users/images", null=True, blank=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(_("email address"), blank=True, null=True, unique=True)
 
-    username_validator = UnicodeUsernameValidator()
-    username = models.CharField(
-        _("username"),
-        max_length=150,
-        unique=True,
-        help_text=_(
-            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
-        ),
-        validators=[username_validator],
-        error_messages={
-            "unique": _("A user with that username already exists."),
-        },
-    )
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), blank=True)
-    is_staff = models.BooleanField(
-        _("staff status"),
-        default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
-    )
-    is_active = models.BooleanField(
-        _("active"),
-        default=True,
-        help_text=_(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
-        ),
-    )
+    image = models.ImageField(upload_to="users/images", null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
 
     objects = MyUserManager()
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["phone"]
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _("user")
