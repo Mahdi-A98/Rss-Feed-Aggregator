bcb366e0d59ce2a8310cb6e3d0ee25f942a06429
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-10-10 23:49:29 +0330

    chore: playlist model to track saved podcasts and eposodes

--- rss_feed/feedback/models.py
+++ rss_feed/feedback/models.py
@@ -17,3 +17,14 @@ class Comment(BaseModel):
 
     def __str__(self):
         return self.account.phone
+class Playlist(BaseModel):
+    title = models.CharField(max_length=40)
+    description = models.TextField(blank=False,null=True)
+    account = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    podcasts = models.ManyToManyField(Podcast)
+    episodes = models.ManyToManyField(Episode)
+
+
+    def __str__(self) -> str:
+        return self.title
