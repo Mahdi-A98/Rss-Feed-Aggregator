8910e97318b07a4fbbfb018130cd42404d3a19c7
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-10-10 23:34:38 +0330

    fix: removing phone from fields and removing redundant configuration

--- rss_feed/account/manager.py
+++ rss_feed/account/manager.py
@@ -9,31 +9,28 @@ from django.apps import apps
 class MyUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self,phone, username, email, password, **extra_fields):
+    def _create_user(self, username, email, password, **extra_fields):
         """
         Create and save a user with the given username, email, and password.
         """
-        if not phone:
-            raise ValueError("The given phone must be set")
+        if not username:
+            raise ValueError("The given username must be set")
         email = self.normalize_email(email)
-        # Lookup the real model class from the global app registry so this
-        # manager method can be used in migrations. This is fine because
-        # managers are by definition working on the real model.
         GlobalUserModel = apps.get_model(
             self.model._meta.app_label, self.model._meta.object_name
         )
         username = GlobalUserModel.normalize_username(username)
-        user = self.model(phone=phone, username=username, email=email, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, phone, username=None, email=None, password=None, **extra_fields):
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(phone=phone, username=username, email=email, password=password, **extra_fields)
+        return self._create_user(username=username, email=email, password=password, **extra_fields)
 
-    def create_superuser(self, phone, username=None, email=None, password=None, **extra_fields):
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -42,31 +39,4 @@ class MyUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(phone=phone,username=username, email=email, password=password, **extra_fields)
-
-    def with_perm(
-        self, perm, is_active=None, include_superusers=True, backend=None, obj=None
-    ):
-        if backend is None:
-            backends = auth._get_backends(return_tuples=True)
-            if len(backends) == 1:
-                backend, _ = backends[0]
-            else:
-                raise ValueError(
-                    "You have multiple authentication backends configured and "
-                    "therefore must provide the `backend` argument."
-                )
-        elif not isinstance(backend, str):
-            raise TypeError(
-                "backend must be a dotted import path string (got %r)." % backend
-            )
-        else:
-            backend = auth.load_backend(backend)
-        if hasattr(backend, "with_perm"):
-            return backend.with_perm(
-                perm,
-                is_active=is_active,
-                include_superusers=include_superusers,
-                obj=obj,
-            )
-        return self.none()
+        return self._create_user(username=username, email=email, password=password, **extra_fields)
