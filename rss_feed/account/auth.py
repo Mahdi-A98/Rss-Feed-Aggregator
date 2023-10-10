2eb5ea69338fb56f64101ce0c843ea308fe258f0
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-10-10 23:32:59 +0330

    chore: complete jwt logics

--- rss_feed/account/auth.py
+++ rss_feed/account/auth.py
@@ -1,77 +1,53 @@
 # In the name of GOD
 
 from django.contrib.auth.backends import ModelBackend
-from datetime import datetime, timedelta
-from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.conf import settings
 
 from rest_framework import authentication
 from rest_framework.exceptions import AuthenticationFailed, ParseError
 
-from jwt import JWT,  jwk_from_dict
+from datetime import datetime, timedelta
+from . import jwt_tools
 
-jwt = JWT()
 User = get_user_model()
 
 class JWTAuthentication(authentication.BaseAuthentication):
-    def authenticate(self, request):
-        # Extract the JWT from the Authorization header
-        jwt_token = request.META.get('HTTP_AUTHORIZATION')
-        if jwt_token is None:
-            return None
+    def authenticate(self, request, refresh_token=None):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            return self.authenticat_with_token(access_token)
+        if refresh_token:
+            return self.authenticat_with_token(refresh_token)
+        raise AuthenticationFailed("Token should be provided")
+        
 
-        jwt_token = JWTAuthentication.get_the_token_from_header(jwt_token)  # clean the token
 
-        # Decode the JWT and verify its signature
-        try:
-            jw_key = jwk_from_dict({'kty':'oct', 'k':settings.SECRET_KEY[:-1]})
-            payload = jwt.decode(jwt_token, jw_key, algorithms=['HS256'])
-        except jwt.exceptions.InvalidSignatureError:
-            raise AuthenticationFailed('Invalid signature')
-        except:
-            raise ParseError()
 
-        # Get the user from the database
-        username_or_phone_number = payload.get('user_identifier')
-        if username_or_phone_number is None:
+    def authenticat_with_token(self, jwt_token):
+        prefix, jwt_token = jwt_token.split()  # clean the token
+        if prefix != settings.JWT_CONF.get("token_prefix"): 
+            raise AuthenticationFailed("Token prefix doesn't match")
+
+        payload, error = jwt_tools.decode_jwt_token(jwt_token)
+        if payload is None:
+            raise error
+        user_id = payload.get('user_identifier')
+        if user_id is None:
             raise AuthenticationFailed('User identifier not found in JWT')
 
-        user = User.objects.filter(username=username_or_phone_number).first()
+        if not jwt_tools.verify_exp(payload): 
+            raise AuthenticationFailed("Token expired")
+        
+        if not user_id == jwt_tools.verify_jti(payload):
+            raise AuthenticationFailed("Invalid user")
+
+        user = User.objects.filter(id=user_id).first()
         if user is None:
-            user = User.objects.filter(phone_number=username_or_phone_number).first()
-            if user is None:
-                raise AuthenticationFailed('User not found')
+            raise AuthenticationFailed('User not found')
 
-        # Return the user and token payload
         return user, payload
-
-    def authenticate_header(self, request):
-        return 'Bearer'
-
-    @classmethod
-    def create_jwt(cls, user, expiration_hours):
-        # Create the JWT payload
-        payload = {
-            'user_identifier': user.username or user.phone or user.email,
-            'exp': int((datetime.now() + timedelta(hours=expiration_hours)).timestamp()),
-            # 'exp': int((datetime.now() + timedelta(hours=settings.JWT_CONF['TOKEN_LIFETIME_HOURS'])).timestamp()),
-            # set the expiration time for 5 hour from now
-            'iat': datetime.now().timestamp(),
-            'username': user.username,
-            'phone_number': user.phone
-        }
-
-        # Encode the JWT with your secret key
-        jw_key = jwk_from_dict({'kty':'oct', 'k':settings.SECRET_KEY[:-1]})
-        jwt_token = jwt.encode(payload, jw_key, alg='HS256')
-        # jwt_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-
-        return jwt_token
-
-    @classmethod
-    def get_the_token_from_header(cls, token):
-        token = token.replace('Bearer', '').replace(' ', '')  # clean the token
-        return token
 
 
 class UserAuthBackend(ModelBackend):
@@ -79,7 +55,7 @@ class UserAuthBackend(ModelBackend):
         username=username
         password=password
         if username is None or password is None:
-            return
+            raise AuthenticationFailed("You should provide credentials !!")
         try:
             user= User.objects.get(Q(email=username) | Q(username=username))
             if user:
@@ -87,23 +63,6 @@ class UserAuthBackend(ModelBackend):
                     return user
         except User.DoesNotExist:
             return None
-
-        # def authenticate(self, request, username=None, password=None, **kwargs):
-        #     if username is None:
-        #         username = kwargs.get(UserModel.USERNAME_FIELD)
-        #     if username is None or password is None:
-        #         return
-        #     try:
-        #         user = UserModel._default_manager.get_by_natural_key(username)
-        #     except UserModel.DoesNotExist:
-        #         # Run the default password hasher once to reduce the timing
-        #         # difference between an existing and a nonexistent user (#20760).
-        #         UserModel().set_password(password)
-        #     else:
-        #         if user.check_password(password) and self.user_can_authenticate(user):
-        #             return user
-
-
 
     def get_user(self, user_id):
         try:
