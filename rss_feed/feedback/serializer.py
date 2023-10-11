832c578c7c00044a41d5b0e9dd04c58c93312100
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-10-11 06:54:06 +0330

    chore: playlist serializer

--- rss_feed/feedback/serializer.py
+++ rss_feed/feedback/serializer.py
@@ -1,3 +1,10 @@
+# In the name of GOD
+
+from rest_framework import serializers
+from .models import Playlist, Like, Comment
+
+
+
 class LikeSerializer(serializers.Serializer):
     model = serializers.CharField(max_length = 50)
     model_id = serializers.IntegerField()
@@ -15,9 +22,26 @@ class LikeSerializer(serializers.Serializer):
                 like = Like(content_object = episode, account = request.user)
                 like.save()
         return like
+
+
 class CommentSerializer(serializers.Serializer):
     model = serializers.CharField(max_length = 50)
     model_id = serializers.IntegerField()
     text = serializers.CharField()
 
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Playlist
+        fields = ['title','description','account','podcasts','episodes']
+
+
+
+    def update(self, instance, validated_data):
+        for podcast in validated_data.get("podcasts"):
+            instance.podcasts.add(podcast)
+        for episode in validated_data.get("episodes"):
+            instance.episodes.add(episode)
+        instance.save()
+        return instance
 
