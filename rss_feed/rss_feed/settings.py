751a6525207e6d041b972478256aec25ba8ad074
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-01 13:15:46 +0330

    chore: adding log middleware

--- rss_feed/rss_feed/settings.py
+++ rss_feed/rss_feed/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.LogMiddleWare',
 ]
 
 ROOT_URLCONF = 'rss_feed.urls'
