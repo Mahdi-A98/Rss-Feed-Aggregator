2f77ec71f4d67caa77ed19c65d86424307f206a4
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-01 13:10:40 +0330

    chore: podcat and its url model serializer

---
+++ rss_feed/podcast/serializer.py
@@ -0,0 +1,16 @@
+# In the name of GOD
+
+from rest_framework.serializers import ModelSerializer
+from .models import Podcast, PodcastUrl
+
+
+class PodcastSerializer(ModelSerializer):
+    class Meta:
+        model = Podcast
+        fields = "__all__"
+
+
+class PodcastUrlSerializer(ModelSerializer):
+    class Meta:
+        model = PodcastUrl
+        fields = ['url','title']
