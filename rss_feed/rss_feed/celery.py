3797c583cad16db1b585121a2fadf931a72b0cc7
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-01 13:13:24 +0330

    fix: correcting typeos

--- rss_feed/rss_feed/celery.py
+++ rss_feed/rss_feed/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGs_MODULE", "rss_feed.settings")
-app = Celery("rss-feed")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rss_feed.settings")
+app = Celery("rss_feed")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
