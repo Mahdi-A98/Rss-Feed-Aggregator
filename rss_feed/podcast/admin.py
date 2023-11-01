9d54a0f8752f508893015caebcc1ff3b757583c6
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-01 13:11:20 +0330

    chore: registering podcast app models in admin panel

--- rss_feed/podcast/admin.py
+++ rss_feed/podcast/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Podcast, Owner, Category, Image, Episode, Generator, PodcastUrl, EpisodeAuthor, PodcastAuthor #, Author, Enclosure
 # Register your models here.
+
+
+admin.site.register(Podcast)
+admin.site.register(Owner)
+# admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Episode)
+# admin.site.register(Enclosure)
+admin.site.register(Generator)
+admin.site.register(PodcastUrl)
+admin.site.register(EpisodeAuthor)
+admin.site.register(PodcastAuthor)
