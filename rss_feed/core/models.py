aa7b2cf983de80d7bb2d3e7515e9d968ae3adc05
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-01 13:09:16 +0330

    chore: user activity log model

--- rss_feed/core/models.py
+++ rss_feed/core/models.py
@@ -1,6 +1,40 @@
 from django.db import models
-
+from account.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+CREATE, READ, UPDATE, DELETE = 'Create', 'Read', 'Update', 'Delete'
+LOGIN, LOGOUT, LOGIN_FAILED = 'Login', 'Logout', 'Login Failed'
+ACTION_TYPES = [
+    (LOGIN, LOGIN),
+    (LOGOUT, LOGOUT),
+    (LOGIN_FAILED, LOGIN_FAILED),
+    (CREATE, CREATE),
+    (READ, READ),
+    (UPDATE, UPDATE),
+    (DELETE, DELETE),
+]
+
+SUCCESS, FAILED = 'Success', 'Failed'
+ACTION_STATUS = ((SUCCESS, "Success"), (FAILED, "Failed"))
+
+
+class ActivityLog(models.Model):
+    actor = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    action_type = models.CharField(choices=ACTION_TYPES, max_length=15)
+    action_time = models.DateTimeField(auto_now_add=True)
+    remarks = models.TextField(null=True, blank=True)
+    status = models.CharField(choices=ACTION_STATUS, max_length=7, default=SUCCESS)
+    data = models.JSONField()
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True, blank=True)
+    object_id = models.PositiveIntegerField(blank=True, null=True)
+    content_object = GenericForeignKey()
+
+    def __str__(self):
+        return f'{self.action_type} by {self.action_type} on {self.action_time}'
