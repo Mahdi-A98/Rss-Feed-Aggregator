2c945f4bf4f9d945ed16bb05d33f6ae7b26e10ca
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-03 16:23:52 +0330

    chore: defining host from settings

--- rss_feed/rss_feed/publisher.py
+++ rss_feed/rss_feed/publisher.py
@@ -1,10 +1,16 @@
 # In the name of GOD
 import pika
 from datetime import datetime
+import os
+import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rss_feed.settings')
+django.setup()
+
+from django.conf import settings
 class Publisher:
     def __init__(self) :
-        self.connection_parameter = pika.ConnectionParameters('localhost')
+        self.connection_parameter = pika.ConnectionParameters(settings.RABBITMQ_HOST)
 
 
     def publish(self, message, queue) :
@@ -12,7 +18,6 @@ class Publisher:
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=queue)
         self.channel.basic_publish(exchange='', routing_key=queue, body='success' + message)
-        print(f'{message} Published Successfully! at {datetime.now()}')
         self.connection.close()
 
     def error_publish(self, message, queue) :
@@ -20,5 +25,4 @@ class Publisher:
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=queue)
         self.channel.basic_publish(exchange='', routing_key=queue, body='error!!' + message)
-        print(f'{message} Published Successfully! at {datetime.now()}')
         self.connection.close()
