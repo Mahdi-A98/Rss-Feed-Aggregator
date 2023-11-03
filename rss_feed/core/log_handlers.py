08eae748b9f18dcc9cc91f43fb225c276b88126a
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-03 16:34:15 +0330

    chore: adding day time suffix to index names

--- rss_feed/core/log_handlers.py
+++ rss_feed/core/log_handlers.py
@@ -12,8 +12,9 @@ class LogSender:
     def __init__(self, elk) -> None:
         self.elk = elk
 
-    def writelog(self, message, formatter, db_name=None) :
-        index_name = db_name or f'log_{time.strftime("%Y_%m_%d")}'
+    def writelog(self, message, formatter, db_name=None, daily_index=False) :
+        suffix = str(time.strftime("%Y_%m_%d")) if daily_index else ""
+        index_name = db_name + suffix or f'log_{time.strftime("%Y_%m_%d")}'
         timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
         log_data = {'message' : formatter(message)}
         log_data['timestamp'] = timestamp
@@ -24,6 +25,7 @@ class LogSender:
 class ElasticHandler(Handler) :
     def __init__(self, host,*args, **kwargs) :
         self.db_name = kwargs.pop('db_name', None)
+        self.daily_index = kwargs.pop('daily_index', False)
         super().__init__(*args, **kwargs)
         self.host = host
         self.elk = Elasticsearch(self.host)
@@ -31,7 +33,7 @@ class ElasticHandler(Handler) :
 
     def emit(self, record) :
         try:
-            self.sender.writelog(record, self.format, db_name=self.db_name)
+            self.sender.writelog(message=record, formatter=self.format, db_name=self.db_name, daily_index=self.daily_index)
         except RecursionError:
             raise
         except Exception:
