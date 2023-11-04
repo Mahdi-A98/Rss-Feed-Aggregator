420a85c17b8d75e06c38193c7de06b0307fbd321
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-04 09:50:41 +0330

    chore: serializers methods updated

--- rss_feed/feedback/serializers.py
+++ rss_feed/feedback/serializers.py
@@ -2,6 +2,7 @@
 
 from rest_framework import serializers
 from .models import Playlist, Like, Comment
+from podcast.models import Podcast, Episode
 
 
 
@@ -29,18 +30,30 @@ class CommentSerializer(serializers.Serializer):
     model_id = serializers.IntegerField()
     text = serializers.CharField()
 
+    def create(self, validated_data):
+        request = self.context['request']
+        if validated_data["model"] == "podcast":
+            podcast = Podcast.objects.get(id=validated_data["model_id"])
+            if podcast:
+                comment = Comment(content_object = podcast, account = request.user, text=validated_data['text'])
+                comment.save()
+        elif validated_data["model"] == "episode":
+            episode = Episode.objects.get(id=validated_data["model_id"])
+            if episode:
+                comment = Comment(content_object = episode, account = request.user, text=validated_data['text'])
+                comment.save()
+        return comment
+
 
 class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
         fields = ['title','description','account','podcasts','episodes']
 
-
-
     def update(self, instance, validated_data):
-        for podcast in validated_data.get("podcasts"):
+        for podcast in validated_data.get("podcasts", []):
             instance.podcasts.add(podcast)
-        for episode in validated_data.get("episodes"):
+        for episode in validated_data.get("episodes", []):
             instance.episodes.add(episode)
         instance.save()
         return instance
