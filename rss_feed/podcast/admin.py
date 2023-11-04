c771bacc0bc7076f228af1676e207a2aad2c9d2f
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-04 15:22:42 +0330

    chore: chainging episode admin interface

--- rss_feed/podcast/admin.py
+++ rss_feed/podcast/admin.py
@@ -8,9 +8,14 @@ admin.site.register(Owner)
 # admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Image)
-admin.site.register(Episode)
+# admin.site.register(Episode)
 # admin.site.register(Enclosure)
 admin.site.register(Generator)
 admin.site.register(PodcastUrl)
 admin.site.register(EpisodeAuthor)
 admin.site.register(PodcastAuthor)
+
+@admin.register(Episode) 
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ["id", "title","pubDate","episode_author",]
+    search_fields = ["title", "description", "id"]
