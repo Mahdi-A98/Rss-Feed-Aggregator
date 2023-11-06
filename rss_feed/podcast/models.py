3028b69704a261d14dacdc5afd1f8497ff5ed4d2
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-06 20:25:56 +0330

    chore: some changes in  models characteristics

--- rss_feed/podcast/models.py
+++ rss_feed/podcast/models.py
@@ -4,10 +4,11 @@ from django.db import models
 from core.models import BaseModel
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
+from datetime import datetime
 
 
 class PodcastAuthor(BaseModel):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
 
     @classmethod
     def convert(cls, converter, save=True, *args, **kwargs):
@@ -24,7 +25,7 @@ class PodcastAuthor(BaseModel):
 
 
 class EpisodeAuthor(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=100, unique=True)
 
     @classmethod
     def convert(cls, converter, save=True, *args, **kwargs):
@@ -54,11 +55,11 @@ class Category(BaseModel):
         return category
 
     def __str__(self):
-        return self.name
+        return f"{self.id} {self.name}"
 
 
 class Owner(BaseModel):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
 
     @classmethod
@@ -82,6 +83,8 @@ class Image(BaseModel):
     url = models.URLField(unique=True, max_length=400)
 
     title = models.CharField(max_length=100,null=True,blank=True)
+    link = models.URLField(unique=True, null=True,blank=True)
+
     @classmethod
     def convert(cls, converter, save=True, *args, **kwargs):
         image_node = converter.get_node('image') or converter.get_node('itunes_image')
@@ -103,7 +106,6 @@ class Image(BaseModel):
 
 class Generator(BaseModel):
     name = models.CharField(max_length=100)
-
     hostname = models.CharField(max_length=150,null=True,blank=True)
     genDate = models.DateTimeField(null=True,blank=True)
 
@@ -116,6 +118,7 @@ class Generator(BaseModel):
             return generator
         generator, created = cls.objects.get_or_create(name=converter.get_attribute('generator'))
         return generator
+
     def __str__(self):
         return self.name
 
@@ -126,25 +129,26 @@ class PodcastUrl(BaseModel):
     is_saved = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.url
+        return f"{self.title}:\t {self.url}"
 
 class Podcast(models.Model):
     title = models.CharField(max_length=100)
     language = models.CharField(max_length=50)
     itunes_type = models.CharField(max_length=50)
     copy_right = models.CharField(max_length=100)
-    explicit = models.CharField(max_length=50)
+    itunes_explicit = models.CharField(max_length=50)
     description = models.TextField() #We use description for itunes summary too.
     pubDate = models.DateTimeField(null=True,blank=True)
-    last_build_date = models.DateTimeField(null=True,blank=True)
+    lastBuildDate = models.DateTimeField(null=True,blank=True)
     link = models.URLField(null=True,blank=True)
     itunes_subtitle = models.TextField(null=True,blank=True)
     itunes_keywords = models.TextField(null=True, blank=True)
-    itunes_image = models.CharField(max_length=400)
+    itunes_image = models.CharField(max_length=400, null=True, blank=True)
     category = models.ManyToManyField(Category)
-    podcast_generator = models.ForeignKey(Generator,on_delete=models.CASCADE,null=True)
-    podcast_author = models.ForeignKey(PodcastAuthor,on_delete=models.CASCADE)
-    podcast_image = models.OneToOneField(Image,on_delete=models.CASCADE)
+    podcast_generator = models.ForeignKey(Generator,on_delete=models.CASCADE,null=True, blank=True)
+    podcast_owner = models.ForeignKey(Owner,on_delete=models.CASCADE,null=True, blank=True)
+    podcast_author = models.ForeignKey(PodcastAuthor,on_delete=models.PROTECT)
+    podcast_image = models.OneToOneField(Image,on_delete=models.PROTECT)
     podcast_url = models.OneToOneField(PodcastUrl, on_delete=models.PROTECT)
 
     @classmethod
@@ -166,21 +170,22 @@ class Podcast(models.Model):
             return podcast
         podcast, created = cls.objects.get_or_create(**deffered_data)
         return podcast
+
     def __str__(self):
         return f"{self.id}: {self.title}"
 
 class Episode(models.Model):
-    title = models.CharField(max_length=100) #We use title for itunes_title too.
-    guid = models.CharField(max_length=150)
+    title = models.CharField(max_length=200) #We use title for itunes_title too.
+    guid = models.CharField(max_length=200)
     itunes_duration = models.CharField(max_length=50)
-    itunes_episode_type = models.CharField(max_length=50)
+    itunes_episodeType = models.CharField(max_length=50)
     itunes_explicit = models.CharField(max_length=50,null=True,blank=True)
     description = models.TextField() #We use description for itunes summary and content_encoded too.
     enclosure = models.CharField(null=True, blank=True,max_length=300)
     link = models.URLField(null=True,blank=True)
     pubDate = models.DateTimeField(null=True,blank=True)
     itunes_keywords = models.TextField(null=True, blank=True)
-    itunes_player = models.CharField(null=True, blank=True,max_length=100) #We use itunes_player for fireside_playerEmbedCode too.
+    itunes_player = models.CharField(null=True, blank=True,max_length=200) #We use itunes_player for fireside_playerEmbedCode too.
     episode_podcast = models.ForeignKey(Podcast,on_delete=models.CASCADE, related_name="episode")
     episode_author = models.ForeignKey(EpisodeAuthor,on_delete=models.CASCADE,null=True)
 
@@ -196,103 +201,6 @@ class Episode(models.Model):
             return episode
         episode, created = cls.objects.get_or_create(**deffered_data)
         return episode
+
     def __str__(self):
         return f"{self.id}: {self.title}"
-
-#******************************************* NEW MODELS ***************************************************
-# class Category(models.Model):
-#     name = models.CharField(max_length=50, unique=True,)
-#     parent_category = models.ForeignKey('self', on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.name
-
-# class Author(models.Model):
-#     name = models.CharField(max_length=100)
-#     email = models.CharField(max_length=100, null=True, blank=True, unique=True)
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     content_object = GenericForeignKey("content_type", "object_id")
-
-
-
-#     def __str__(self):
-#         return f"{self.id}: {self.name}"
-
-
-# class Owner(models.Model):
-#     name = models.CharField(max_length=50)
-#     email = models.EmailField(null=True, blank=True, unique=True)
-
-#     def __str__(self):
-#         return f"{self.id}: {self.name}"
-
-# class Image(models.Model):
-#     title = models.CharField(max_length=100, null=True, blank=True)
-#     link = models.URLField(null=True, blank=True)
-#     url = models.URLField()
-
-#     def __str__(self):
-#         return f"{self.id}: {self.title}"
-
-# class Generator(models.Model):
-#     name = models.CharField(max_length=100)
-#     hostname = models.CharField(max_length=150, null=True, blank=True)
-#     genDate = models.DateTimeField(null=True, blank=True)
-
-#     def __str__(self):
-#         return f"{self.id}: {self.name}"
-
-
-# class Enclosure(models.Model):
-#     url = models.URLField()
-#     length = models.PositiveIntegerField(null=True, blank=True)
-#     c_type = models.CharField(max_length=30, null=True, blank=True)
-
-
-# class Podcast(BaseModel):
-#     """
-#         required tags are title, description, image, language, category and explicit
-#     """
-#     title = models.CharField(max_length=100)
-#     description = models.TextField() #We use description for itunes summary too.
-#     image = models.OneToOneField(Image,on_delete=models.PROTECT)
-#     language = models.CharField(max_length=50)
-#     category = models.ManyToManyField(Category)
-#     explicit = models.CharField(max_length=50)
-#     itunes_type = models.CharField(max_length=50, null=True, blank=True)
-#     copy_right = models.CharField(max_length=100, null=True, blank=True)
-#     pubDate = models.DateTimeField(null=True,blank=True)
-#     last_build_date = models.DateTimeField(null=True,blank=True)
-#     link = models.URLField(null=True,blank=True)
-#     itunes_subtitle = models.TextField(null=True,blank=True)
-#     itunes_keywords = models.TextField(null=True, blank=True)
-#     generator = models.ForeignKey(Generator,on_delete=models.CASCADE, null=True, blank=True)
-#     author = GenericRelation(Author)
-#     # (Author,on_delete=models.SET_NULL, null=True, blank=True)
-
-#     def __str__(self):
-#         return f"{self.id}: {self.title}"
-
-
-# class Episode(models.Model):
-#     """
-#         required tags are title, enclosure
-#     """
-#     episode_podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=100) #We use title for itunes_title too.
-#     enclosure = models.ForeignKey(Enclosure, on_delete=models.CASCADE)
-#     guid = models.CharField(max_length=400, null=True, blank=True)
-#     itunes_duration = models.CharField(max_length=50, null=True, blank=True)
-#     itunes_episode_type = models.CharField(max_length=50, null=True, blank=True)
-#     itunes_explicit = models.CharField(max_length=50, null=True, blank=True)
-#     description = models.TextField(null=True, blank=True) #We use description for itunes summary and content_encoded too.
-#     link = models.URLField(null=True,blank=True)
-#     pub_date = models.DateTimeField(null=True,blank=True)
-#     itunes_keywords = models.TextField(null=True, blank=True)
-#     itunes_player = models.CharField(max_length=50, null=True, blank=True) #We use itunes_player for fireside_playerEmbedCode too.
-#     author = GenericRelation(Author)
-#     # author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True)
-
-#     def __str__(self):
-#         return f"{self.id}: {self.title}"
