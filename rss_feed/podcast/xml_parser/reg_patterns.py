29def1852853c329a8697d138b0969bedd54a1f0
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-11-06 20:28:18 +0330

    chore: xml parser regex patterns

---
+++ rss_feed/podcast/xml_parser/reg_patterns.py
@@ -0,0 +1,17 @@
+# In the name of GOD
+
+import re
+
+item_tag_pattern = re.compile("<item>((\n|.)*?)</item>")
+tag_pattern = re.compile("<([^\<\>\[\]\- ]*)(\s*(\S*)=\"([^\"]*?)\"[^\>\]\[]*)*>")
+key_value_pair_pattern = re.compile('([^ ]*?)=\"(.*?)\"')
+tag_value_pattern = "(?:<tag.*?>)(.*?)(?:<\/tag>)"
+self_close_value_pattern = "(?:<tag[^\<\>\[\]\- ]*)([^\<\>\[\]]*?)(?:/>)"
+
+tag_attr_value = "<(?P<tag>[^\/\<\>\[\]\- ]{1,})(?P<attribute>\s*((\S*)=\"([^\"]*?)\")*?[^\>\]\[]*)*?>(?P<value>[^\<\>]*)"
+new_t = "<(?P<tag>[^\/\<\>\[\]\- ]{1,})(?P<attribute>\s*((\S*)=\"([^\"]*?)\")*?[^\>\]\[]*)>(?P<value>[^@]*?)</(?P=tag)>"
+
+# \s+[^\<\>\[\]]*?)\/>
+
+# <(?P<tag>[^\/<>\[\]\s]{1,})(?P<attribute>\s+[^<>\[\]\s-]+=\"[^\"]*\")*>(?P<value>[\d\w\n\s\S]*?)<\/(?P=tag)>|(<(?P<self_tag>[^\/<>\[\]\s-]{1,})(?P<self_attribute>\s+[^<>\[\]\s]+=\"[^\"]*\")*\/>)
+
